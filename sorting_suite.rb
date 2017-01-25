cc16c5cdb836070a9e527eed83ab2f3f2199596f
Christopher Dale <epiphone2903@hotmail.com>
2017-01-25 12:03:57 -0700

    class work

---
+++ sorting_suite.rb
@@ -0,0 +1,14 @@
+class BubbleSort
+  def sort(collection)
+    # is there a way where ruby understands what the oder of the alphabet?
+    # I want to try to make a library of the alphabet, get the index positions of that array and compare it to the current given array.
+    "a" > "b" returns a false value, so ruby does recognize the alphapbet and that is has a value.
+    # use if & elsif statements to move the index positions around.
+  end
+end
+
+sorter = BubbleSort.new
+
+sorter.sort(["d", "b", "a", "c"])
+# i need to take this array and turn it inro this ["a", "b", "c", "d"]
+I could try to look and see if ruby knows
